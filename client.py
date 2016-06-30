b76c51acb85bc8767a9076cd802e2739a74c8423
land-pack <landpack@sina.com>
2016-06-30 04:33:39 -0400

    add error handler for socket() call

--- client.py
+++ client.py
@@ -1,4 +1,9 @@
 import socket 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+import sys
+try:
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+except socket.error,msg:
+	print 'Failed to create socket. Error code: ' + str(msg[0]) + ' ,Error message : '
+	sys.exit();
 print 'Socket Created'
 
